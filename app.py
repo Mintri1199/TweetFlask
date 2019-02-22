e698f0753bf155faf901fc976c8e90219b4ba1d1
Jackson Ho <mintri1199@gmail.com>
2019-02-21 19:40:19 -0800

    deploy?

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
 def index():
     start = t.Markov(c.clean_up_text('queen_reign.txt'))
     start.getting_types()
     start.logic()
     sentence = start.multiple_runs(random.randint(5 , 15))
 
-    return render_template("index.html", title=None, sentence=sentence) 
+    return render_template("index.html", title=None, sentence=sentence)
+
+
 
 
